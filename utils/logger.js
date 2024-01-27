cc1de659cb0f8f04d007e5dc24b44c0632a23069
Pavel Abramchuk <pa@ithillel.ua>
2024-01-27 23:09:58 +0200

    hw_2 | fixes, improvements

--- utils/logger.js
+++ utils/logger.js
@@ -1,26 +1,28 @@
 const colors = require('colors');
 const config = require('config');
 
-const is_enabled = Number(config.get('COLORS_ENABLED')) === 1;
-const log_level = config.get('LOG_LEVEL');
+const logLevel = config.get('LOG_LEVEL');
+
+if (Number(config.get('COLORS_ENABLED')) === 0) {
+    colors.disable();
+}
 
 function logger(name) {
     name = `${name}:`;
-    
+
     const noop = () => { };
-    const info = log_level === 'info' ? console.log : noop;
-    const warn = ['info', 'warn'].includes(log_level) ? console.warn : noop;
-    const error = ['info', 'warn', 'error'].includes(log_level) ? console.error : noop;
+    const info = logLevel === 'info' ? console.log : noop;
+    const warn = ['info', 'warn'].includes(logLevel) ? console.warn : noop;
 
     return {
         info: (...payload) => {
-            info(is_enabled ? colors.green(name) : name, ...payload)
+            info(colors.green(name), ...payload)
         },
         warn: (...payload) => {
-            warn(is_enabled ? colors.yellow(name) : name, ...payload)
+            warn(colors.yellow(name), ...payload)
         },
         error: (...payload) => {
-            error(is_enabled ? colors.red(name) : name, ...payload)
+            console.error(colors.red(name), ...payload)
         },
     }
 }
