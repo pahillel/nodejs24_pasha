660023e9db8a87395df02f16da8724e5d3c74e82
Pavel Abramchuk <pa@ithillel.ua>
2024-02-03 14:28:41 +0200

    hw_3 | restore

--- utils/logger.js
+++ utils/logger.js
@@ -4,27 +4,27 @@ const config = require('config');
 const logLevel = config.get('LOG_LEVEL');
 
 if (Number(config.get('COLORS_ENABLED')) === 0) {
-    colors.disable();
+  colors.disable();
 }
 
 function logger(name) {
-    name = `${name}:`;
+  name = `${name}:`;
 
-    const noop = () => { };
-    const info = logLevel === 'info' ? console.log : noop;
-    const warn = ['info', 'warn'].includes(logLevel) ? console.warn : noop;
+  const noop = () => {};
+  const info = logLevel === 'info' ? console.log : noop;
+  const warn = ['info', 'warn'].includes(logLevel) ? console.warn : noop;
 
-    return {
-        info: (...payload) => {
-            info(colors.green(name), ...payload)
-        },
-        warn: (...payload) => {
-            warn(colors.yellow(name), ...payload)
-        },
-        error: (...payload) => {
-            console.error(colors.red(name), ...payload)
-        },
+  return {
+    info: (...payload) => {
+      info(colors.green(name), ...payload);
+    },
+    warn: (...payload) => {
+      warn(colors.yellow(name), ...payload);
+    },
+    error: (...payload) => {
+      console.error(colors.red(name), ...payload);
     }
+  };
 }
 
 module.exports = logger;
