3db203bdfefbd48ba5aecd5b088f12f46bc77591
Pavel Abramchuk <pa@ithillel.ua>
2024-02-22 00:05:03 +0200

    hw_6 | validation errors

--- api/middlewares.js
+++ api/middlewares.js
@@ -1,4 +1,5 @@
-const { celebrate } = require('celebrate');
+const { celebrate, isCelebrateError } = require('celebrate');
+const { statusCodes } = require('./constants');
 
 const validate = (
   schema,
@@ -10,7 +11,19 @@ const validate = (
   },
   celebrateOptions = {}
 ) => {
-  return celebrate(schema, options, celebrateOptions);
+  return (req, res, next) => {
+    celebrate(schema, options, celebrateOptions)(req, res, (err) => {
+      if (err && isCelebrateError(err)) {
+        const error = Array.from(err.details.entries()).map(
+          ([key, detail]) => `Validation error (${key}): ${detail.message}`
+        );
+
+        return res.status(statusCodes.BAD_REQUEST).json({ error });
+      }
+
+      next();
+    });
+  };
 };
 
 const response = (res, statusCode, data) => {
