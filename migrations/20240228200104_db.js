1e226333c2dc379971d185fcc47e6ab621bae536
Pavel Abramchuk <pa@ithillel.ua>
2024-02-28 22:59:45 +0200

    hw_7 | config, knex config, migration

---
+++ migrations/20240228200104_db.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable('users', (table) => {
+    table.increments();
+    table.string('name', 64).notNullable();
+    table.string('email', 64).notNullable();
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable('users');
+};
